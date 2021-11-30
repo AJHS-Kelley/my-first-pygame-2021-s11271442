517c560864c55b4a8efd96e8830d4fde2f7efb9d
Isiah C <70644169+s11271442@users.noreply.github.com>
2021-11-30 14:48:12 -0500

    v0.5
    Drew the game background

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Isiah Carter, 11/30/21, 2:43PM, v0.4
+# My First PyGame, Isiah Carter, 11/30/21, 2:47PM, v0.5
 
 import pygame, sys
 from pygame.locals import *
@@ -26,3 +26,6 @@ text = basicFont.render('Hello, world!', True, WHITE, BLUE)
 textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx
 textRect.centery = windowSurface.get_rect().centery
+
+# Draw the game background.
+windowSurface.fill(INDIGO)
