8f15fbf0438b6c752987d3c2984b2c09ad2c7fc1
Isiah C <70644169+s11271442@users.noreply.github.com>
2021-11-30 14:34:57 -0500

    v0.3
    Added color values

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Isiah Carter, 11/30/21, 2:25PM, v0.2
+# My First PyGame, Isiah Carter, 11/30/21, 2:34PM, v0.3
 
 import pygame, sys
 from pygame.locals import *
@@ -9,3 +9,11 @@ pygame.init()
 # Setup the game window
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption('Hello, world!')
+
+# Setup color values.
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+INDIGO = (75, 0, 130)
