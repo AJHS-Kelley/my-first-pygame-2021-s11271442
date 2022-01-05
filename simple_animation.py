b88e2fb39eb8168f120fe44477563090bfed90eb
Isiah C <70644169+s11271442@users.noreply.github.com>
2022-01-05 09:39:52 -0500

    v0.8
    Draw the box onto the game surface
    
    Draw the window Surface

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Isiah Carter, 1/5/22, 9:35AM, v0.7
+# Simple Animation with PyGame, Isiah Carter, 1/5/22, 9:39AM, v0.8
 
 import pygame, sys, time
 from pygame.locals import *
@@ -81,4 +81,12 @@ while True:
                     b['dir'] = DOWNLEFT
                 if b['dir'] == UPRIGHT:
                     b['dir'] = UPLEFT
+
+        # Draw the box onto the game surface.
+        pygame.draw.rect(windowSurface, b['color'], b['rect'])
+
+    # Draw the window to the screen.
+    pygame.display.update()
+    time.sleep(0.02)
+
  
