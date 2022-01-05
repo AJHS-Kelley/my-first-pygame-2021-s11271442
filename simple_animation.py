38d717ec915d70f782211414a620df4e593d573b
Isiah C <70644169+s11271442@users.noreply.github.com>
2022-01-05 09:00:02 -0500

    v0.2
    Established directions for DIAGNOAL movement.
    
    Established the MOVESPEED.

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Isiah Carter, 1/5/22, 8:53AM, v0.1
+# Simple Animation with PyGame, Isiah Carter, 1/5/22, 8:59AM, v0.2
 
 import pygame, sys, time
 from pygame.locals import *
@@ -11,3 +11,11 @@ WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation Example!')
+
+#Setup the direction variable
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4
