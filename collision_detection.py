172ed4e9c5bc850c0a225f51f8c7d2cf51d67fe5
Isiah C <70644169+s11271442@users.noreply.github.com>
2022-01-13 08:34:42 -0500

    v0.2
    Setup Pygame window.

--- collision_detection.py
+++ collision_detection.py
@@ -1,4 +1,4 @@
-#PyGame Collision Practice, Ryan Kelley, January 04, 2022, 8:29am, v0.1
+#PyGame Collision Practice, Ryan Kelley, January 04, 2022, 8:33am, v0.2
 
 import pygame, sys, random
 from pgame.locals import *
@@ -6,3 +6,9 @@ from pgame.locals import *
 #Setup Pygame
 pygame.init()
 mainClock = pygame.time.Clock()
+
+#Setup the Pygame Window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Collision Detection 2022')
